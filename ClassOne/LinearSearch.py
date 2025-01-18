5d46f57d464f6455267a9d4e18bfc056954b5d79
Juan Solo <J2AN.S0l0@gmail.com>
2025-01-18 13:26:53 -0600

    O(n) Linear Complexity

--- ClassOne/LinearSearch.py
+++ ClassOne/LinearSearch.py
@@ -2,16 +2,14 @@
 Linear Search O(n)
 '''
 
-def linear_search(array, value):
+# O(n): 1 + 1 + 1 + n + n + 1 + 1
 
-    for i in range(len(array)):
-        if array[i] == value:
-            return True
-        
-    return False
+array = [6] * 100               # O(1)
+value = 9                       # O(1)
+value_in_array = False          # O(1)
 
-n = 100
-test_array = [7] * n
-test_value = 8
+for i in range(len(array)):     # O(n)
+    if array[i] == value:       # O(n)
+        value_in_array = True   # O(1)
 
-print(linear_search(test_array, test_value))
+print(value_in_array)           # O(1)
