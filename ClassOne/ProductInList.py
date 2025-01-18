889aff4472a385deed23fcb1f88131bef3db5d1c
Juan Solo <J2AN.S0l0@gmail.com>
2025-01-18 13:10:34 -0600

    Complexity O(n^2)

---
+++ ClassOne/ProductInList.py
@@ -0,0 +1,14 @@
+'''
+Product in List O(n^2)
+'''
+
+array = [1] * 100                           # O(1)
+product = 2                                 # O(1)
+product_in_array = False                    # O(1)
+
+for i in range(len(array)):                 # O(n)
+    for j in range(len(array)):             #   O(n)
+        if array[i] * array[j] == product:  #   O(n)
+            product_in_array = True         #   O(1)
+
+print(product_in_array)                     # O(1)
