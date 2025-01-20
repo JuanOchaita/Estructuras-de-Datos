4b597023965de9027e48ef70f15253476890b5b5
Juan Solo <J2AN.S0l0@gmail.com>
2025-01-20 10:54:34 -0600

    Test Fake Stack Overflow

---
+++ ClassThree/stack_test.py
@@ -0,0 +1,22 @@
+"""
+Stack Testing
+"""
+
+from stack import Stack
+
+stest_stack = Stack(5)
+print(stest_stack)
+
+#Pushes
+stest_stack.Push("A")
+print(stest_stack)
+stest_stack.Push("B")
+print(stest_stack)
+stest_stack.Push("C")
+print(stest_stack)
+stest_stack.Push("D")
+print(stest_stack)
+stest_stack.Push("E")
+print(stest_stack)
+stest_stack.Push("F")
+print(stest_stack)
