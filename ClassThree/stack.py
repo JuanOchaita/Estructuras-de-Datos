3326ba9656f6c3fb26fc4c39cb6cb85fc86a0e81
Juan Solo <J2AN.S0l0@gmail.com>
2025-01-23 19:06:24 -0600

    Stack | Push Pop Peek

--- ClassThree/stack.py
+++ ClassThree/stack.py
@@ -4,25 +4,39 @@ Stack Implementation
 
 class Stack:
     def __init__(self, size: int):
+        
         self.Top = -1
         self.Max = size
         self.Elements = [None] * size
 
     def __repr__(self):
+        
         return f"Current stack: {self.Elements} | Top: {self.Top}"
 
     def Push(self, value: int) -> None:
+        
         if self.Top == self.Max -1:
             print("OVERFLOW :/")
-        else:
-            self.Top += 1
-            self.Elements[self.Top] = value
+            return None
     
+        self.Top += 1
+        self.Elements[self.Top] = value
+
     def Pop(self) -> any:
+        
         if self.Top == -1:
             print("Stack Underflow")
-        else:
-            value = self.Elements[self.Top]
-            self.Elements[self.Top]
-            self.Top -= 1
-            return value
+            return None
+    
+        value = self.Elements[self.Top]
+        self.Elements[self.Top] = None
+        self.Top -= 1
+        return value
+
+    def peek(self) -> any:
+        
+        if self.Top == -1:
+            print("Stack underflow")
+            return None
+        
+        return self.Elements[self.Top]
